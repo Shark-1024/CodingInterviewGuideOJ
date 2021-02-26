9382a8ff9195eb37abd28779fe023d888ca446aa
hanlin.zhou <hanlin.zhou@11bee.com>
2021-02-26 16:41:22 +0800

    a

---
+++ src/main/java/linkedList/Palindrome.java
@@ -0,0 +1,38 @@
+package linkedList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+/**
+ * @author hanlin.zhou
+ * @date 2021/2/26 0:03
+ */
+public class Palindrome {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+        int len = Integer.parseInt(b.readLine());
+        String[] readLine = b.readLine().trim().split(" ");
+        b.close();
+        System.out.println(isPalindromeByStack(Node.buildLinkedList(readLine)));
+    }
+
+    private static boolean isPalindromeByStack(Node head){
+        Node cur = head;
+        Stack<Node> stack = new Stack<>();
+        while(cur != null){
+            stack.push(cur);
+            cur = cur.next;
+        }
+        while(head != null){
+            if (head.value != stack.pop().value){
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+
+}
